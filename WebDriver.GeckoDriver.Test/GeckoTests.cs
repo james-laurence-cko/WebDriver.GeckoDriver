36afda5af5e560ac9e6bc18afc1268638d56f49a
James Laurence <cdt_laurence@hotmail.co.uk>
2016-11-09 12:50:24 +0000

    Renamed wires.exe to geckodriver.exe

---
+++ WebDriver.GeckoDriver.Test/GeckoTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Firefox;
+
+namespace WebDriver.GeckoDriver.Test
+{
+    [TestClass]
+    public class GeckoTests
+    {
+        [TestMethod]
+        public void GeckoTest()
+        {
+            using (var driver = new FirefoxDriver { Url = "https://www.google.com" })
+            {
+                driver.Close();
+            }
+        }
+    }
+}
