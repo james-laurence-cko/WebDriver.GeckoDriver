9d5a2512a311dd3718dfb62383e0479462fe3b24
James Laurence <cdt_laurence@hotmail.co.uk>
2018-01-08 10:25:02 +0000

    Updated geckodriver to V0.19.1

--- WebDriver.GeckoDriver/Properties/AssemblyInfo.cs
+++ WebDriver.GeckoDriver/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("James Laurence")]
 [assembly: AssemblyProduct("WebDriver.GeckoDriver")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.16.1")]
-[assembly: AssemblyFileVersion("0.16.1")]
+[assembly: AssemblyVersion("0.19.1")]
+[assembly: AssemblyFileVersion("0.19.1")]
